d77f037a7afdd850ca8351051d08851d8bed01f4
annalittler <77206754+annalittler@users.noreply.github.com>
2022-08-26 12:33:40 +1200

    fixed routing

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,37 +1,17 @@
 import "./App.css";
-import { useState } from "react";
-import Header from "./components/header/Header";
-import SearchBar from "./components/search-bar/SearchBar";
-import BodyPlaceholder from "./components/body-placeholder/BodyPlaceholder";
-import Modal from "./components/modal/Modal";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Homepage from "./pages/Homepage";
 
 function App() {
-  const [modalState, setModalState] = useState(false);
-  const [searchResults, setSearchResults] = useState(null);
-  const [inputQuery, setInputQuery] = useState(undefined);
-
   return (
-    <div className="App">
-      {modalState === true && (
-        <Modal
-          modalState={modalState}
-          setModalState={setModalState}
-          searchResults={searchResults}
-          setSearchResults={setSearchResults}
-          inputQuery={inputQuery}
-          setInputQuery={setInputQuery}
-        />
-      )}
-      <Header
-        inputQuery={inputQuery}
-        setInputQuery={setInputQuery}
-        searchResults={searchResults}
-        setSearchResults={setSearchResults}
-        modalState={modalState}
-        setModalState={setModalState}
-      />
-      <BodyPlaceholder />
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/home" element={<Homepage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ front-end/src/pages/Homepage.js
@@ -0,0 +1,37 @@
+import "../App.css";
+import { useState } from "react";
+import Header from "../components/header/Header";
+import BodyPlaceholder from "../components/body-placeholder/BodyPlaceholder";
+import Modal from "../components/modal/Modal";
+
+function Homepage() {
+  const [modalState, setModalState] = useState(false);
+  const [searchResults, setSearchResults] = useState(null);
+  const [inputQuery, setInputQuery] = useState(undefined);
+
+  return (
+    <div className="Homepage">
+      {modalState === true && (
+        <Modal
+          modalState={modalState}
+          setModalState={setModalState}
+          searchResults={searchResults}
+          setSearchResults={setSearchResults}
+          inputQuery={inputQuery}
+          setInputQuery={setInputQuery}
+        />
+      )}
+      <Header
+        inputQuery={inputQuery}
+        setInputQuery={setInputQuery}
+        searchResults={searchResults}
+        setSearchResults={setSearchResults}
+        modalState={modalState}
+        setModalState={setModalState}
+      />
+      <BodyPlaceholder />
+    </div>
+  );
+}
+
+export default Homepage;
