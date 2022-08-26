ae797b0a95e48d5693153a94a2bea4b5513f4eb2
annalittler <77206754+annalittler@users.noreply.github.com>
2022-08-26 12:37:37 +1200

    trying to fix netlify deploy issue

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -8,7 +8,6 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Homepage />} />
-          <Route path="/home" element={<Homepage />} />
         </Routes>
       </BrowserRouter>
     </>
