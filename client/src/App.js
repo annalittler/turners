c6eb130500a9cb16d1959cf9a1886ed8bf0b082d
Vin274 <101485986+Vin274@users.noreply.github.com>
2022-09-06 00:12:31 +1200

    created progress bar component

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,24 @@
-import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Homepage from "./pages/Homepage";
+import "./App.css"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Homepage from "./pages/Homepage"
+import CustomerReviews from "./pages/CustomerReviews"
+import QuoteStep2 from "./pages/QuoteStep2"
 
 function App() {
-  return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-        </Routes>
-      </BrowserRouter>
-    </>
-  );
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Homepage />} />
+                    <Route
+                        path="/customer-reviews"
+                        element={<CustomerReviews />}
+                    />
+                    <Route path="/quote-step-2" element={<QuoteStep2 />} />
+                </Routes>
+            </BrowserRouter>
+        </>
+    )
 }
 
-export default App;
+export default App
