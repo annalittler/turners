7a8057f8276b8566267601f3b58639c2d798a084
annalittler <77206754+annalittler@users.noreply.github.com>
2022-09-09 12:05:16 +1200

    add table

---
+++ client/src/components/bottom-nav/BottomNav.js
@@ -0,0 +1,45 @@
+import React from "react";
+import "./BottomNav.scss";
+import { col1, col2, col3, col4 } from "./NavItems";
+import { Link } from "react-router-dom";
+
+export default function BottomNav() {
+  return (
+    <>
+      <div className="bottom-nav">
+        <section>
+          <h4>CARS</h4>
+          {col2.map((item) => (
+            <Link to={item.link} className="bottom-nav-item">
+              {item.name}
+            </Link>
+          ))}
+        </section>
+        <section>
+          <h4>FINANCE &amp; INSURANCE</h4>
+          {col2.map((item) => (
+            <Link to={item.link} className="bottom-nav-item">
+              {item.name}
+            </Link>
+          ))}
+        </section>
+        <section>
+          <h4>SEARCH FOR</h4>
+          {col3.map((item) => (
+            <Link to={item.link} className="bottom-nav-item">
+              {item.name}
+            </Link>
+          ))}
+        </section>
+        <section>
+          <h4>ABOUT US</h4>
+          {col4.map((item) => (
+            <Link to={item.link} className="bottom-nav-item">
+              {item.name}
+            </Link>
+          ))}
+        </section>
+      </div>
+    </>
+  );
+}
---
+++ client/src/components/bottom-nav/NavItems.js
@@ -0,0 +1,135 @@
+export const col1 = [
+  {
+    name: "How to Buy",
+    link: "",
+  },
+  {
+    name: "Sell Your Car",
+    link: "",
+  },
+  {
+    name: "Finance & Insurance",
+    link: "",
+  },
+  {
+    name: "Auctions & Events",
+    link: "",
+  },
+  {
+    name: "Buyer and Seller Fees",
+    link: "",
+  },
+  {
+    name: "Vehicle Sale Price History Tool",
+    link: "",
+  },
+  {
+    name: "Shipping Costs",
+    link: "",
+  },
+];
+
+export const col2 = [
+  {
+    name: "Finance Homepage",
+    link: "",
+  },
+  {
+    name: "Car & Personal Finance",
+    link: "",
+  },
+  {
+    name: "Loan Calculator",
+    link: "",
+  },
+  {
+    name: "Car Insurance",
+    link: "/insurance",
+  },
+  {
+    name: "Mechanical Breakdown Insurance",
+    link: "",
+  },
+  {
+    name: "General Insurances",
+    link: "",
+  },
+  {
+    name: "Trucks Finance",
+    link: "",
+  },
+  {
+    name: "Financial Information",
+    link: "",
+  },
+];
+
+export const col3 = [
+  {
+    name: "Cars",
+    link: "",
+  },
+  {
+    name: "Trucks & Machinery",
+    link: "",
+  },
+  {
+    name: "Damaged & End of Life Cars",
+    link: "",
+  },
+  {
+    name: "Boats & Marine",
+    link: "",
+  },
+  {
+    name: "Motorcycles & Scooters",
+    link: "",
+  },
+  {
+    name: "General Goods",
+    link: "",
+  },
+  {
+    name: "Buses, Caravans & Motorhomes",
+    link: "",
+  },
+  {
+    name: "Turners Auction Schedule",
+    link: "",
+  },
+];
+
+export const col4 = [
+  {
+    name: "Overview",
+    link: "",
+  },
+  {
+    name: "Turners Careers",
+    link: "",
+  },
+  {
+    name: "Terms and Conditions",
+    link: "",
+  },
+  {
+    name: "Privacy Policy",
+    link: "",
+  },
+  {
+    name: "Turners Live",
+    link: "",
+  },
+  {
+    name: "The Good Oil Blog",
+    link: "",
+  },
+  {
+    name: "Email Alerts",
+    link: "",
+  },
+  {
+    name: "Contact Us",
+    link: "",
+  },
+];
